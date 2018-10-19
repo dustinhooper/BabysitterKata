3705ad8f3898307c55b42225c0e2d4c7ef547e7e
Dustin Hooper <dustin.hooper@gmx.com>
2018-10-19 16:41:06 -0700

    Created an object called BabysitterTime to hold all the times. Created a method stub called GetNightlyCharge, in the TimeCalcController.

---
+++ OHGBabysitterKata/OHGBabysitterKata/Controllers/TimeCalcController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OHGBabysitterKata.Models;
+
+namespace OHGBabysitterKata.Controllers
+{
+    public class TimeCalcController : ApiController
+    {
+        public decimal GetNightlyCharge(BabysitterTime BTimeObj)
+        {
+            return 0.0M;
+        }
+    }
+}
---
+++ OHGBabysitterKata/OHGBabysitterKata/Models/BabysitterTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OHGBabysitterKata.Models
+{
+    public class BabysitterTime
+    {
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public DateTime Bedtime { get; set; }
+    }
+}
